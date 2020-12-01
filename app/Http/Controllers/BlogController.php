8c80c235bff41d74cea85053bcbb65dbfd57a5e4
$amalBoii <49010379+lloydskie78@users.noreply.github.com>
2020-12-01 10:22:31 +0800

    Made the ff changes: - Added routing throughout the site - Made a view all blog page - Made a tag and category individual page - Added view composer on the AppServiceProvider ;

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Blog;
 use App\Category;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -12,15 +13,59 @@ class BlogController extends Controller
     {
         $categories = Category::select('id', 'categoryName')->get();
 
-        $blogs = Blog::orderBy('id', 'desc')->with(['cat', 'user'])->limit(6)->get(['id', 'title', 'post_excerpt', 'slug', 'user_id', 'featuredImage']);
+        $blogs = Blog::orderBy('id', 'desc')
+            ->with(['cat', 'user'])
+            ->limit(6)
+            ->get(['id', 'title', 'post_excerpt', 'slug', 'user_id', 'featuredImage']);
         // return $blogs;
         return view('home')->with(['categories' => $categories, 'blogs' => $blogs]);
     }
 
     public function blogSingle(Request $request, $slug)
     {
-        $categories = Category::select('id', 'categoryName')->get();
-        $blog = Blog::where('slug', $slug)->with(['cat', 'tag', 'user'])->first(['id', 'title', 'user_id', 'featuredImage', 'post']);
-        return view('blogsingle')->with(['categories' => $categories, 'blog' => $blog]);
+        $blog = Blog::where('slug', $slug)
+            ->with(['cat', 'tag', 'user'])
+            ->first(['id', 'title', 'user_id', 'featuredImage', 'post']);
+
+        $category_ids = [];
+        foreach ($blog->cat as $cat) {
+            array_push($category_ids, $cat->id);
+        }
+
+        $relatedBlogs = Blog::with('user')->where('id', '!=', $blog->id)->whereHas('cat', function ($q) use ($category_ids) {
+            $q->whereIn('category_id', $category_ids);
+        })->limit(5)->orderBy('id', 'desc')->get(['id', 'title', 'slug', 'user_id', 'featuredImage']);
+
+        return view('blogsingle')->with(['blog' => $blog, 'relatedBlogs' => $relatedBlogs]);
+    }
+
+    public function compose(View $view)
+    {
+        $cat = Category::select('id', 'categoryName')->get(['id', 'categoryName']);
+        $view->with('cat', $cat);
+    }
+
+    public function categoryIndex(Request $request, $categoryName, $id)
+    {
+        $blogs = Blog::with('user')->whereHas('cat', function ($q) use ($id) {
+            $q->where('category_id', $id);
+        })->orderBy('id', 'desc')->select(['id', 'title', 'slug', 'user_id', 'featuredImage'])->paginate(1);
+        return view('category')->with(['categoryName' => $categoryName, 'blogs' => $blogs]);
+    }
+    public function tagIndex(Request $request, $tagName, $id)
+    {
+        $blogs = Blog::with('user')->whereHas('tag', function ($q) use ($id) {
+            $q->where('tag_id', $id);
+        })->orderBy('id', 'desc')->select(['id', 'title', 'slug', 'user_id', 'featuredImage'])->paginate(1);
+        return view('tag')->with(['tagName' => $tagName, 'blogs' => $blogs]);
+    }
+
+    public function allBlogs(Request $request)
+    {
+        $blogs = Blog::orderBy('id', 'desc')
+            ->with(['cat', 'user'])
+            ->select(['id', 'title', 'post_excerpt', 'slug', 'user_id', 'featuredImage'])
+            ->paginate(1);
+        return view('blogs')->with(['blogs' => $blogs]);
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -7,20 +7,15 @@
 				<div class="row">
 					<div class="col-12 col-md-10 col-lg-8">
 						<div class="row">
-							@if (count($categories)>0)
-								@foreach ($categories as $nav)
 									<div class="col-12 col-md-4 col-lg-4">
 									<a href="">
 										<div class="banner_box">
 											<i class="fab fa-laravel"></i>
-											<h3 class="banner_box_h3">{{ $nav->categoryName }}</h3>
-											{{-- <p>The Toptal Blog is the top hub for developers.</p> --}}
+											<h3 class="banner_box_h3">{{ $categoryName }}</h3>
+											<p>The Toptal Blog is the top hub for developers.</p>
 										</div>
 									</a>
 							</div>
-								@endforeach
-							@endif
-							
 						</div>
 					</div>
 				</div>
@@ -37,14 +32,14 @@
 					</div>
 					<div class="row">
 
+
 						@if (count($blogs)>0)
 							@foreach ($blogs as $b)
-
 								<div class="col-12 col-md-6 col-lg-4">
 									<a href="/blog/{{ $b->slug }}">
 										<div class="home_card">
 											<div class="home_card_top">
-												<img src="img/card3.jpg" alt="image">
+												<img src="../../img/card3.jpg" alt="image">
 											</div>
 											<div class="home_card_bottom">
 												<div class="home_card_bottom_text">
@@ -52,13 +47,13 @@
 														<ul class="home_card_bottom_text_ul">
 															@foreach ($b->cat as $c)
 																<li>
-																	<a href="blog_post.html">{{ $c->categoryName }}</a>
+																	<a href="/blog/{{ $b->slug }}">{{ $c->categoryName }}</a>
 																	{{-- <span><i class="fas fa-angle-right"></i></span> --}}
 																</li>
 															@endforeach
 														</ul>
 													@endif
-													<a href="blog_post.html">
+													<a href="/blog/{{ $b->slug }}">
 														<h2 class="home_card_h2">{{ $b->title }}</h2>
 													</a>
 													<p class="post_p">
@@ -66,7 +61,7 @@
 													</p>
 													<div class="home_card_bottom_tym">
 														<div class="home_card_btm_left">
-															<img src="img/man1.jpg" alt="image">
+															<img src="../../img/man1.jpg" alt="image">
 														</div>
 														<div class="home_card_btm_r8">
 														<a href="contact_me.html"><p class="author_name">{{ $b->user->fullName }}</p></a>
@@ -84,10 +79,11 @@
 
 							@endforeach
 						@endif
-					</div>
-					<div class="text-center">
-						<button><a href="/blogs">View all</a></button>
-					</div>
+
+                               
+						
+                    </div>
+                    {!! $blogs->links() !!}
 				</div>
 			</div>
 		</div>
